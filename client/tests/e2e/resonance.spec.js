6f3bab4bbaef70d2916cf0c93ac95f4fa96c90d2
Nina Peters <peters.nina@outlook.de>
2020-12-10 14:06:07 +0100

    Update cypress configs

---
+++ client/tests/e2e/resonance.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+const testId = (id) => `[data-testid="${id}"]`
+
+describe('resonance', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  describe('resonance', () => {})
+})
