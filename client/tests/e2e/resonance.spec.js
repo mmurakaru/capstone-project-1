5c1c5e3a06616f9efc72cc690bc9407a7f843603
Nina Peters <peters.nina@outlook.de>
2020-12-10 16:42:01 +0100

    Move eslintrc to test folder and update cypress test

--- client/tests/e2e/resonance.spec.js
+++ client/tests/e2e/resonance.spec.js
@@ -3,13 +3,20 @@
 const testId = (id) => `[data-testid="${id}"]`
 
 describe('resonance', () => {
-  beforeEach(() => {
+  it('should open the login page by first load', () => {
     cy.visit('/')
   })
 
-  describe('resonance', () => {
-    it('should open the login page by first load', () => {
-      cy.visit('/')
-    })
+  it('should display a login button', () => {
+    cy.get('[data-testid=login-button]')
   })
+
+  /*   it('should lead to homepage by clicking the login button', () => {
+    cy.get('[data-testid=login-button]')
+      .should('have.attr', 'href')
+      .and('include', 'login')
+      .then((href) => {
+        cy.visit(href)
+      })
+  }) */
 })
